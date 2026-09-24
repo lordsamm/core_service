f09ccad4ff6e70b1741cfce474f89084e5e59f6c
lordsamm <lordsamm@github.com>
2026-09-24 06:08:23 -0500

    use plain Activity instead of AppCompatActivity

--- app/src/main/java/com/example/coreservice/MainActivity.kt
+++ app/src/main/java/com/example/coreservice/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.coreservice
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // no setContentView, no nothing
     }
 }
